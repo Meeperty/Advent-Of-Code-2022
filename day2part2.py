3e606a73ac36997a2102b289192e19dc290cfd4f
Meeperty <meepprograms@gmail.com>
2022-12-03 10:42:28 -0800

    day 2 done

--- day2part2.py
+++ day2part2.py
@@ -8,20 +8,20 @@ input_file = open("day2.txt")
 total_points = 0
 for line in input_file:
     opponent_move = line[0]
-    my_move = line[2]
+    desired_result = line[2]
     
     points = 0
     match opponent_move:
         case 'A':
-            match my_move:
+            match desired_result:
                 case 'X':
-                    points += 3 + 1
-                case 'Y':
-                    points += 6 + 2
-                case 'Z':
                     points += 0 + 3
+                case 'Y':
+                    points += 3 + 1
+                case 'Z':
+                    points += 6 + 2
         case 'B':
-            match my_move:
+            match desired_result:
                 case 'X':
                     points += 0 + 1
                 case 'Y':
@@ -29,14 +29,14 @@ for line in input_file:
                 case 'Z':
                     points += 6 + 3
         case 'C':
-            match my_move:
+            match desired_result:
                 case 'X':
-                    points += 6 + 1
-                case 'Y':
                     points += 0 + 2
-                case 'Z':
+                case 'Y':
                     points += 3 + 3
+                case 'Z':
+                    points += 6 + 1
     
-    print(" ".join([str(opponent_move), str(my_move), str(points)]))
+    #print(" ".join([str(opponent_move), str(desired_result), str(points)]))
     total_points += points
 print(total_points)
