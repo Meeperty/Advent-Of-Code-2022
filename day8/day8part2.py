f55f1a07bcac8f0244bd23940099ca07a0ab1079
Meeperty <meepprograms@gmail.com>
2022-12-08 22:03:12 -0800

    day 8 done

--- day8/day8part2.py
+++ day8/day8part2.py
@@ -9,41 +9,48 @@ def get_column(c : int, l : list):
 min = 0
 max = 99
 
-sum = 0
+highest_score = 0
 trees = list()
 for r in range(max):
-    tree_line = list()
+    line = list()
     text_line = input_file.readline()
     for c in range(max):
-        tree_line.append(text_line[c])
-    trees.append(tree_line)
+        line.append(int(text_line[c]))
+    trees.append(line)
 
 for r in range(0, max):
     for c in range(0, max):
-        if r != min and r != max - 1 and c != min and c != max - 1:
-            row = trees[r]
-            col = get_column(c, trees)
-            this_height = trees[r][c]
-            earlier_in_row, later_in_row = row[:c], row[c + 1:]
-            earlier_in_col, later_in_col = col[:r], col[r + 1:]
-            visible_left, visible_right, visible_up, visible_down = True, True, True, True
-            for height in earlier_in_row:
-                if height >= this_height:
-                    visible_left = False
-            for height in later_in_row:
-                if height >= this_height:
-                    visible_right = False
-            for height in earlier_in_col:
-                if height >= this_height:
-                    visible_up = False
-            for height in later_in_col:
-                if height >= this_height:
-                    visible_down = False
-            if visible_left or visible_right or visible_up or visible_down:
-                sum += 1
-                print(this_height + " at " + str(r) + ", " + str(c) + " is visible")
-        else:
-            sum += 1
-            print(trees[r][c] + " at " + str(r) + ", " + str(c) + " is visible")
+        row = trees[r]
+        col = get_column(c, trees)
+        this_height = trees[r][c]
+        
+        earlier_in_row, later_in_row = row[:c], row[c + 1:]
+        earlier_in_col, later_in_col = col[:r], col[r + 1:]
+        earlier_in_row = list(reversed(earlier_in_row))
+        earlier_in_col = list(reversed(earlier_in_col))
+        
+        visible_left, visible_right, visible_up, visible_down = 0, 0, 0 ,0
+        
+        for h in earlier_in_row:
+            visible_left += 1
+            if h >= this_height:
+                break
+        for h in later_in_row:
+            visible_right += 1
+            if h >= this_height:
+                break
+        for h in earlier_in_col:
+            visible_up += 1
+            if h >= this_height:
+                break
+        for h in later_in_col:
+            visible_down += 1
+            if h >= this_height:
+                break
+        visibility_score = visible_left * visible_right * visible_up * visible_down
+        
+        print("height " + str(this_height) + " at " + str(r) + ", " + str(c) + " has " + str(visibility_score))
+        if (visibility_score > highest_score):
+            highest_score = visibility_score
 
-print(sum)
+print(highest_score)
