59dc9b369fd2fdffa3e698c563d649e22dd95232
Meeperty <meepprograms@gmail.com>
2022-12-10 10:21:18 -0800

    start of 5

--- day5/day5.py
+++ day5/day5.py
@@ -1,6 +1,15 @@
-input_file = open("day5.txt")
+import collections
+
+input_file = open("/Users/brothers/source/Advent-Of-Code-2022/day5/day5.txt")
 
 sum = 0
 line = input_file.readline()
+setup_lines = list()
+num_stacks = 9
+#get the first few lines that have initial position
 while line != '\n':
-    
+    setup_lines.append(line)
+
+setup_lines.reverse()
+
+print(sum)
